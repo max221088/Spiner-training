3eafeb3d6e98b3012c0bcca536c7795f80349269
max221088 <max.work221088@gmail.com>
2023-01-22 23:26:07 +0200

    add first function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const $spinV1 = document.querySelector(".element");
 let startTime = new Date().getTime();
-let hightEl = 50;
+let hightEl = null;
 console.log(startTime);
 function spin1() {
 	let curentTime = new Date().getTime();
-	hightEl = ((((curentTime - startTime) / 1000) * 100) % 130) + 50;
+	//hightEl = ((((curentTime - startTime) / 1000) * 100) % 130) + 50;
+	hightEl = Math.sin((((curentTime - startTime) / 1000) * 2) % 90);
+	hightEl = Math.abs(hightEl) * 100 + 50;
 	console.log(hightEl);
 	$spinV1.style.cssText = "height: " + hightEl + "px;";
 	requestAnimationFrame(spin1);
