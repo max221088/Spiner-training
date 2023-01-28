8babfd87ecd234f1b6c66feb16a7b43cd3d8a6d0
max221088 <117590988+max221088@users.noreply.github.com>
2023-01-28 21:43:08 +0200

    Add all dalay

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-const $spinV1 = document.querySelector(".element");
+const $spinV1 = document.querySelectorAll(".element");
+console.log($spinV1);
 let startTime = new Date().getTime();
 let hightEl = null;
-console.log(startTime);
+let hightEl2 = [];
+
 function spin1() {
 	let curentTime = new Date().getTime();
-	//hightEl = ((((curentTime - startTime) / 1000) * 100) % 130) + 50;
-	hightEl = Math.sin((((curentTime - startTime) / 1000) * 2) % 90);
-	hightEl = Math.abs(hightEl) * 100 + 50;
-	console.log(hightEl);
-	$spinV1.style.cssText = "height: " + hightEl + "px;";
+	hightEl2 = [];
+	for (let i = 0; i < $spinV1.length; i++) {
+		let dal = 250 * i;
+		hightEl = Math.sin((((curentTime - startTime + dal) / 1000) * 2) % 90);
+		hightEl = Math.abs(hightEl) * 100 + 50;
+		hightEl2.push(hightEl);
+		//console.log(hightEl2);
+	}
+	$spinV1[0].style.cssText = "height: " + hightEl2[0] + "px;";
+	$spinV1[1].style.cssText = "height: " + hightEl2[1] + "px;";
+	$spinV1[2].style.cssText = "height: " + hightEl2[2] + "px;";
+	$spinV1[3].style.cssText = "height: " + hightEl2[3] + "px;";
+	$spinV1[4].style.cssText = "height: " + hightEl2[4] + "px;";
 	requestAnimationFrame(spin1);
 }
 spin1();
